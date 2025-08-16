462117d0311cfb1a867d5fbdaa2d6d13548d12c4
Claas <claas@claas.dev>
2025-08-16 17:59:39 +0200

    Fix graph

--- src/Debug.tsx
+++ src/Debug.tsx
@@ -1,81 +1,14 @@
-import {
-  createEffect,
-  createResource,
-  createSignal,
-  ErrorBoundary,
-  For,
-  Show,
-} from "solid-js";
+import { createResource, ErrorBoundary } from "solid-js";
 import { openDatabase } from "./data";
 import Graph from "./Graph";
-
-type Unit = "kg" | "lb";
 
 export default function Debug() {
   const [database] = createResource(openDatabase);
 
-  const [entries, { refetch }] = createResource(database, async (database) => {
+  const [entries] = createResource(database, async (database) => {
     const entries = await database.getAll("entries");
 
     return entries;
-  });
-
-  async function handleSubmit(
-    event: SubmitEvent & { currentTarget: HTMLFormElement }
-  ) {
-    event.preventDefault();
-    console.debug("handleSubmit", event);
-
-    const input = event.currentTarget.elements.namedItem("weight");
-    if (!(input instanceof HTMLInputElement))
-      throw new Error("Expected input element");
-
-    const weight = input.valueAsNumber;
-
-    const currentDatabase = database();
-    console.debug("currentDatabase", currentDatabase);
-    const now = new Date();
-    currentDatabase?.add(
-      "entries",
-      {
-        timestamp: now,
-        weight,
-      },
-      now.getTime()
-    );
-    event.currentTarget.reset();
-    refetch();
-  }
-
-  async function remove(date: Date) {
-    const currentDatabase = database();
-    if (!currentDatabase) throw new Error("Expected database to be ready");
-
-    currentDatabase.delete("entries", date.getTime());
-    refetch();
-  }
-
-  const [unit, setUnit] = createSignal<Unit>("kg");
-
-  function format(weight: number, unit: Unit) {
-    if (unit === "kg") return weight;
-    // If 1lb = 0.45_359_237kg
-    // Then divide both sides by 45359237 to get 1/45_359_237lb and 0.00_000_001kg
-    // Multiply both sides by 100_000_000 to get 2.2_046_226_218lbs = 1kg
-    // 1/45_359_237*100_000_000
-    return weight * 2.2046226218;
-  }
-
-  let weightInput: HTMLInputElement | undefined;
-
-  createEffect(() => {
-    // Need to access unit here to register it as a effect dependency
-    const currentUnit = unit();
-    if (!weightInput?.valueAsNumber) return;
-    weightInput.valueAsNumber =
-      currentUnit === "kg"
-        ? weightInput.valueAsNumber / 2.2046226218
-        : weightInput.valueAsNumber * 2.2046226218;
   });
 
   return (
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,22 +1,12 @@
-import { createEffect, For, Show, type Accessor, type JSX } from "solid-js";
+import { For, Show, type Accessor, type JSX } from "solid-js";
 import type { Entry } from "./data";
 
-function* repeat(times: number) {
-  for (let index = 0; index < times; index++) {
-    yield index;
-  }
+function startOfMonth(date: Date) {
+  return new Date(date.getFullYear(), date.getMonth(), 1);
 }
 
-function daysInMonth(year: number, month: number) {
-  // You can go negative on days. So 0 is the last day of the previous month
-  // Momth is 0 based index e.g. 11 is December
-  // Sou we would have to -1 to get the current month with the momth number
-  // But we want the next one so it would be +1 which cancels out the -1
-  return new Date(year, month, 0).getDate();
-}
-
-function startOfMonth(year: number, month: number) {
-  return new Date(year, month, 1);
+function endOfMonth(date: Date) {
+  return new Date(date.getFullYear(), date.getMonth() + 1, 0);
 }
 
 type Properties = { entries: Accessor<Entry[]> };
@@ -25,11 +15,6 @@ type TrendLine = Pick<
   JSX.LineSVGAttributes<SVGLineElement>,
   "x1" | "x2" | "y1" | "y2"
 >;
-
-type Max = {
-  x: number;
-  y: number;
-};
 
 type Range = {
   from: number;
@@ -145,11 +130,6 @@ function useTrendLine(entries: Accessor<Entry[]>): {
   return { trendLine, aggregation };
 }
 
-type Point = {
-  x: number;
-  y: number;
-};
-
 function XAxisMarks({ height }: { height: number }) {
   return (
     <For each={[10, 20, 30, 40, 50, 60, 70, 80, 90]}>
@@ -166,7 +146,7 @@ function XAxisMarks({ height }: { height: number }) {
   );
 }
 
-function YAxisMarks({ width }: { width: number }) {
+function YAxisMarks() {
   return (
     <For each={[10, 20, 30, 40, 50, 60, 70, 80, 90]}>
       {(y) => (
@@ -183,103 +163,76 @@ function YAxisMarks({ width }: { width: number }) {
   );
 }
 
-// const adjustToSvgSpace = () => {
-//   const { x, y } = ranges();
-//   const maxX = x.to;
-//   const maxY = y.to;
-
-//   return (x: number, y: number) => {
-//     // Percent of the max value
-//     // Percent of the width
-//     x = (x / maxX) * width;
-//     y = (y / maxY) * height;
-
-//     return [height - y, width - x];
-//   };
-// };
-
-function adjustToSvgSpace(
-  x: number,
-  y: number,
-  maxX: number,
-  maxY: number,
-  width: number,
-  height: number
-) {
-  // Percent of the max value
-  // Percent of the width
-  x = (x / maxX) * width;
-  y = (y / maxY) * height;
-
-  console.debug({ x, y, maxX, maxY, width, height });
-
-  return [height - y, width - x];
-}
-
 export default function Graph({ entries }: Properties) {
   //TODO make range configurable
   // Need to curb time stamp or we get overflow
   const now = new Date();
-  const xRange = {
-    from: startOfMonth(now.getFullYear(), now.getMonth()).getTime(),
-    to: Number.POSITIVE_INFINITY,
-  };
-  const { trendLine, aggregation } = useTrendLine(entries);
+
+  const timeStart = startOfMonth(now).getTime();
+  const timeEnd = endOfMonth(now).getTime();
+
+  // const weightStart = 0;
+  const weightEnd = 100;
+
+  const { trendLine } = useTrendLine(entries);
 
   const width = 100;
   const height = 100;
+  const adjustX = (x: number) => {
+    const timeRange = timeEnd - timeStart;
+    const xInTimeRange = x - timeStart;
+    const percentage = xInTimeRange / timeRange;
 
-  createEffect(() => console.debug("trendLine", trendLine()));
+    const inSvg = percentage * width;
+    return inSvg;
+  };
+
+  const adjustY = (y: number) => {
+    const percentage = y / weightEnd;
+    const inSvg = height - percentage * height;
+    return inSvg;
+  };
 
   return (
-    <Show when={aggregation().ranges}>
-      {(ranges) => {
-        const adjust = (x: number, y: number) => {
-          const { x: xRange } = ranges();
-          const relativeX = (x / xRange.to) * 100;
-
-          return adjustToSvgSpace(relativeX, y, 100, 100, width, height);
-        };
-        return (
-          <svg viewBox={`0 0 ${width} ${height}`}>
-            <YAxisMarks width={width} />
-            <XAxisMarks height={height} />
-            <Show when={trendLine()}>
-              {(trendLine) => {
-                const line = trendLine();
-                const [x1, y1] = adjust(line.x1 as number, line.x2 as number);
-                const [x2, y2] = adjust(line.x2 as number, line.x2 as number);
-                return (
-                  <line
-                    x1={x1}
-                    x2={x2}
-                    y1={y1}
-                    y2={y2}
-                    stroke-linecap="round"
-                    class="stroke-[0.5] stroke-sky-400"
-                  />
-                );
-              }}
-            </Show>
-            <For each={entries()}>
-              {({ weight, timestamp }) => {
-                const [x, y] = adjust(timestamp.getTime(), weight);
-                return (
-                  // TODO filter out values out of range
-                  <circle
-                    cx={x}
-                    cy={y}
-                    r="1"
-                    data-weight={weight}
-                    class="fill-emerald-500"
-                  />
-                );
-              }}
-            </For>
-            <circle cx="50" cy="50" r="1" class="fill-emerald-500" />
-          </svg>
-        );
-      }}
-    </Show>
+    <svg viewBox={`0 0 ${width} ${height}`}>
+      <YAxisMarks />
+      <XAxisMarks height={height} />
+      <Show when={trendLine()}>
+        {(trendLine) => {
+          const { x1, x2, y1, y2 } = trendLine();
+          return (
+            <line
+              x1={adjustX(x1 as number)}
+              x2={adjustX(x2 as number)}
+              y1={adjustY(y1 as number)}
+              y2={adjustY(y2 as number)}
+              stroke-linecap="round"
+              class="stroke-[0.5] stroke-sky-400"
+            />
+          );
+        }}
+      </Show>
+      <For each={entries()}>
+        {({ weight, timestamp }) => {
+          return (
+            // TODO filter out values out of range
+            <circle
+              cx={adjustX(timestamp.getTime())}
+              cy={adjustY(weight)}
+              r="1"
+              data-weight={weight}
+              class="fill-emerald-500"
+            />
+          );
+        }}
+      </For>
+      <path
+        d={`M${width / 2 - 2.5},${height / 2} l5,0 M${width / 2},${
+          height / 2 - 2.5
+        } l0,5 `}
+        stroke-width={0.5}
+        class="stroke-gray-300"
+      />
+    </svg>
   );
 }
